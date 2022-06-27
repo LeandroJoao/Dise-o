7457f03d770d1aefe792f95956c74df9794d1017
marzzioChicana <89101069+marzzioChicana@users.noreply.github.com>
2022-06-27 10:30:30 -0500

    Add files via upload

---
+++ src/mothers/application/dtos/request/update-mother-request.dto.ts
@@ -0,0 +1,9 @@
+export class UpdatePadresRequestDto {
+  constructor(
+    public readonly id: number,
+    public readonly name: string,
+    public readonly lastName: string,
+    public readonly password: string,
+    public readonly email: string
+  ) {}
+}
